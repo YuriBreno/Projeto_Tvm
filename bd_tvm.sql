0365495abbd2b6e21fa200081024703d51c472f4
Yuri Breno <90795682+YuriBreno@users.noreply.github.com>
2022-05-17 21:13:31 -0300

    Update and rename bd_tvm (1).sql to bd_tvm.sql

--- bd_tvm.sql
+++ bd_tvm.sql
@@ -1,35 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 18-Maio-2022 às 02:07
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Banco de dados: `bd_tvm`
 --
-CREATE DATABASE IF NOT EXISTS `bd_tvm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE  `bd_tvm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `bd_tvm`;
-
--- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE `cliente` (
   `nome` varchar(60) DEFAULT NULL,
   `data_criacao` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
@@ -47,7 +25,6 @@ CREATE TABLE `cliente` (
 -- Estrutura da tabela `funcionarios`
 --
 
-DROP TABLE IF EXISTS `funcionarios`;
 CREATE TABLE `funcionarios` (
   `nome_usuario` varchar(60) DEFAULT NULL,
   `email` varchar(20) NOT NULL,
@@ -63,13 +40,10 @@ CREATE TABLE `funcionarios` (
 --       `usuario` -> `nome_usuario`
 --
 
--- --------------------------------------------------------
-
 --
 -- Estrutura da tabela `problemas`
 --
 
-DROP TABLE IF EXISTS `problemas`;
 CREATE TABLE `problemas` (
   `codigo_problema` int(60) NOT NULL,
   `status_problema` varchar(25) NOT NULL,
@@ -90,7 +64,6 @@ CREATE TABLE `problemas` (
 -- Estrutura da tabela `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `nome_usuario` varchar(60) NOT NULL,
   `senha` varchar(60) DEFAULT NULL
@@ -156,7 +129,3 @@ ALTER TABLE `funcionarios`
 ALTER TABLE `problemas`
   ADD CONSTRAINT `problemas_ibfk_1` FOREIGN KEY (`codigo_cliente`) REFERENCES `cliente` (`codigo_cliente`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
